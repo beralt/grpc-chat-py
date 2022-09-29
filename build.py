c74c31cf37aa95e2f912742b897227758bdab773
Beralt Meppelink <beralt.meppelink@alten.nl>
2022-09-29 14:54:54 +0200

    add chat service to server

--- build.py
+++ build.py
@@ -2,13 +2,19 @@ from grpc_tools import protoc
 from pathlib import Path
 
 topdir = Path(__file__).parent
-protosdir = topdir / "protos"
-protofile = topdir / "protos" / "greet.proto"
 
-protoc.main((
-    "",
-    f"-I{protosdir}",
-    f"--python_out={topdir}",
-    f"--grpc_python_out={topdir}",
-    str(protofile)
-))
+protosdir = topdir / "protos"
+
+protofiles = [
+    str(topdir / "protos" / "greet.proto"),
+    str(topdir / "protos" / "chat.proto"),
+]
+
+for protofile in protofiles:
+    protoc.main((
+        "",
+        f"-I{protosdir}",
+        f"--python_out={topdir}",
+        f"--grpc_python_out={topdir}",
+        protofile
+    ))
--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 from concurrent import futures
+
 from greet_pb2_grpc import GreeterServicer, add_GreeterServicer_to_server
 from greet_pb2 import HelloRequest, HelloReply
+
+from chat_pb2_grpc import ChatServicer, add_ChatServicer_to_server
+from chat_pb2 import ChatMessage
+
 import grpc
 import logging
 
@@ -14,10 +19,21 @@ class Service(GreeterServicer):
         reply.message = f"Hello to you {request.name}!"
         return reply
 
+class ChatService(ChatServicer):
+    def __init__(self) -> None:
+        pass
+
+    def ChitChat(self, request_iterator, context):
+        for new_message in request_iterator:
+            msg = ChatMessage()
+            msg.message = f"Did you just say '{new_message.message}'?"
+            yield msg
+
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     add_GreeterServicer_to_server(Service(), server)
+    add_ChatServicer_to_server(ChatService(), server)
     server.add_insecure_port("[::]:5556")
     server.start()
     server.wait_for_termination()
