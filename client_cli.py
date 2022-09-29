d17c3f48a8fc1ea52b2701f101f85a2d4cb441ad
Beralt Meppelink <beralt.meppelink@alten.nl>
2022-09-29 14:55:13 +0200

    add chat service to commandline client

--- client_cli.py
+++ client_cli.py
@@ -1,6 +1,27 @@
 import grpc
+
 from greet_pb2_grpc import GreeterStub
 from greet_pb2 import HelloRequest, HelloReply
+
+from chat_pb2_grpc import ChatStub
+from chat_pb2 import ChatMessage
+
+# a simple generator for messages
+def create_messages():
+    msgs = [
+        "can",
+        "I",
+        "haz",
+        "msgs",
+        "plz",
+        "k",
+        "thnx",
+        "bye"
+    ]
+    for m in msgs:
+        msg = ChatMessage()
+        msg.message = m
+        yield msg
 
 if __name__ == "__main__":
     with grpc.insecure_channel("localhost:5556") as channel:
@@ -9,3 +30,8 @@ if __name__ == "__main__":
         request.name = "Botty"
         response: HelloReply = stub.SayHello(request)
         print(response.message)
+
+        chat_stub = ChatStub(channel)
+        responses = chat_stub.ChitChat(create_messages())
+        for response in responses:
+            print(f"got msg back: {response.message}")
